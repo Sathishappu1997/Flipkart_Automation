6abb9e4171564e10c102a07b0ae7abd948e3f576
sathishappu1997 <sathishappu1997@gmail.com>
2025-06-20 21:10:16 +0530

    Report path changed

--- src/main/java/com/flipkart/constant/ConstantVariables.java
+++ src/main/java/com/flipkart/constant/ConstantVariables.java
@@ -7,7 +7,7 @@ public class ConstantVariables {
 	public static final String EXPECTEDHOMETITLE = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
 	public static final String EXPECTEDRESULTTITLE = "Iphone- Buy Products Online at Best Price in India - All Categories | Flipkart.com";
 	public static final String PROPERTYFILEPATH = System.getProperty("user.dir")+File.separator+"PropertyFile"+File.separator+"BasicInput.properties";
-	public static final String EXTENTREPORTFILEPATH = System.getProperty("user.dir")+File.separator+"ExtentHTMLReport"+File.separator+"index.html";
+	public static final String EXTENTREPORTFILEPATH = System.getProperty("user.dir")+File.separator+"test-output"+File.separator+"ExtentHTMLReport"+File.separator+"index.html";
 	public static final String EXCELFILEPATH = System.getProperty("user.dir")+File.separator+"ExcelInput"+File.separator+"InputData.xlsx";
 
 }
